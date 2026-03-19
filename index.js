c9e1bc870853d4d49d161e1a026cf8961ac50a8a
shauryaverma03 <shauryaverma036@gmail.com>
2026-03-20 00:51:31 +0530

    added some more pages and error page

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
+let data = require('./data/users.json');
+app.get("/instagram", (req, res) => {
+    res.render('index', { data: data });
+});
+
+app.use((req, res) => {
+    res.render('error');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
